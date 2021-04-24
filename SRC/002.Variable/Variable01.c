537e6aba1eec2a41ddbefe6445bc394b70c8e64d
hyunsu <gustn0289@naver.com>
2021-04-24 08:46:45 +0000

    변수 선언

---
+++ SRC/002.Variable/Variable01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	a=10;
+	printf("a 의 값은 : %d \n",a); //%d 정수형 변수를 출력할 때 
+	
+	return 0;
+	//구문오류(syntex error)
+	//논리오류(logical error)
+	//(type error??)
+}
