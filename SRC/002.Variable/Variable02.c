1d2feae12e3992bb52251b6dba6601d8379a9649
hyunsu <gustn0289@naver.com>
2021-04-24 08:48:10 +0000

    문구 수정

--- SRC/002.Variable/Variable02.c
+++ SRC/002.Variable/Variable02.c
@@ -4,9 +4,9 @@ int main()
 {
 	int a= 127;
 	
-	printf("a의 값은 %d 진수로 %o 입니다.\n", 8,a);
-	printf("a의 값은 %d 진수로 %d 입니다.\n", 10,a);
-	printf("a의 값은 %d 진수로 %x 입니다.\n", 16,a);
+	printf("%d의 값은 %d 진수표현은 %o 입니다.\n",a, 8,a);
+	printf("%d의 값은 %d 진수표현은 %d 입니다.\n",a, 10,a);
+	printf("%d의 값은 %d 진수표현은 %x 입니다.\n",a, 16,a);
 	
 	return 0;
 }
