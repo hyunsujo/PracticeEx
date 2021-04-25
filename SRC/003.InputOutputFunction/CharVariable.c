9950e02f64446a6e71ed2ec263e5c166363ab744
hyunsu <gustn0289@naver.com>
2021-04-25 07:04:46 +0000

    문자형 변수 선언

---
+++ SRC/003.InputOutputFunction/CharVariable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	char a;
+	a= 'q';
+	
+	//printf("%d, %c \n" ,a ,a);
+	
+	printf("%c의 ASCII Code 값이 %d 입니다.\n",a,a);
+	
+	//printf("'a'의 값과 저장되어 있는 문자는 ? 값 : %d, 문자 : %c \n",a,a);
+	
+	return 0;
+}
