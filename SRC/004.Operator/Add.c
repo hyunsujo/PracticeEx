c355166a35c5c9168d410a93eaadc61b76e9c92d
hyunsu <gustn0289@naver.com>
2021-04-25 08:33:28 +0000

    덧셈 연산자

---
+++ SRC/004.Operator/Add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=1, b=1, c=1 ,d=1;
+	
+	a= a+1;
+	printf("a : %d \n",a);
+	
+	b += 1; //b = b+1 과 같다
+	printf("b : %d \n",b);
+	
+	++c;
+	printf("c : %d \n",c);
+	d++;
+	printf("d : %d \n",d);
+	
+	return 0;
+}
