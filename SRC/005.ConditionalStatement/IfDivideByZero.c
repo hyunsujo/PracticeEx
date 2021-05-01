746d9a40c56f126a44700741d865dd3c22742cde
hyunsu <gustn0289@naver.com>
2021-05-01 06:22:41 +0000

    0으로 나눌시

---
+++ SRC/005.ConditionalStatement/IfDivideByZero.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	double i, j;
+    printf("나눗셈을 하고 싶은 2개의 수 중 첫번째 수를 입력하세요 : " );
+	scanf("%lf", &i);
+	printf("나눗셈을 하고 싶은 2개의 수 중 두번째 수를 입력하세요 : " );
+    scanf("%lf", &j);
+		  
+    if ( j== 0)
+		  {
+			  printf("0으로 나눌수 없습니다.\n");
+			  return 0;
+		  }
+	printf("%f를 %f로 나눈 결과는 : %f \n",i ,j ,i/j);
+		  
+    return 0;
+}
